ddb17995d6fe00c1c8f1a395cc17101cdf008550
Robin Lake <robinhlake@gmail.com>
2023-06-18 19:37:07 -0700

    tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_client(mut stream: TcpStream) {
+    println!("New client! {}", stream.peer_addr().unwrap());
+    let mut result: [u8; 128] = [0; 128];
+    // result = stream.read(&mut [0; 128]).unwrap();
+    stream.read(&mut result).unwrap();
+    println!("result: {:?}", result);
+    stream.write(b"Hello Peer!\r\n").unwrap();
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -18,13 +31,20 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    // let _ = HttpServer::new(|| {
+    //     App::new()
+    //         .service(hello)
+    //         .service(echo)
+    //         .route("/hey", web::get().to(manual_hello))
+    // })
+    // .bind(("192.168.50.20", 8080))?
+    // .run()
+    // .await;
+    let listener = TcpListener::bind("192.168.50.20:8081").unwrap();
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
 }
