19d0f2bd5efc13b13f745601d6c78307ba2d6a8f
Max Edmands <max@honeycomb.io>
2023-06-22 22:43:39 -0700

    Adds error handling

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,32 @@
 use std::{
     fs::OpenOptions,
-    io::{copy, BufReader, Write},
+    io::{copy, BufReader},
     net::{TcpListener, TcpStream},
     thread,
 };
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(stream: TcpStream) {
     let mut file = OpenOptions::new()
         .append(true)
         .create(true)
         .open("gpslog.txt")
         .expect("Unable to open file");
+
     let mut reader = BufReader::new(&stream);
-    copy(&mut reader, &mut file).expect("Unable to write data");
-    stream
-        .write(b"Hello Peer!\r\n")
-        .expect("unable to respond to client");
-    stream.flush().unwrap();
+    match copy(&mut reader, &mut file) {
+        Ok(_) => return,
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::ConnectionReset => return,
+            _ => eprintln!("Unable to copy data: {}, {}", e.kind(), e),
+        },
+    }
 }
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:8080").expect("couldn't start TCP server");
+    let listener = TcpListener::bind("0.0.0.0:8084").expect("couldn't start TCP server");
     // accept connections and process them serially
     for stream in listener.incoming() {
-        thread::spawn(|| {
-            // connection succeeded
-            let stream = stream.expect("Unable to unwrap stream");
-            handle_client(stream);
-        });
-        // handle_client(stream?);
+        thread::spawn(|| handle_client(stream.expect("Unable to unwrap stream")));
     }
     Ok(())
 }
