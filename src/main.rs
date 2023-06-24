44e95ec858fe2c877509084998f9bad1ab028a3c
Max Edmands <max@honeycomb.io>
2023-06-24 15:32:39 -0700

    command line args and logs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use pardini::ThreadPool;
 use std::{
     fs::OpenOptions,
@@ -6,6 +7,13 @@ use std::{
 };
 
 fn handle_client(stream: TcpStream) {
+    let peer_name = stream
+        .peer_addr()
+        .and_then(|ok| Ok(ok.to_string()))
+        .unwrap_or("Unknown".to_string());
+
+    log::debug!("client {} connected", peer_name);
+
     let mut file = OpenOptions::new()
         .append(true)
         .create(true)
@@ -14,17 +22,42 @@ fn handle_client(stream: TcpStream) {
 
     let mut reader = BufReader::new(&stream);
     match copy(&mut reader, &mut file) {
-        Ok(_) => return,
+        Ok(_) => log::debug!("client {} disconnected", peer_name),
         Err(e) => match e.kind() {
-            std::io::ErrorKind::ConnectionReset => return,
-            _ => eprintln!("Unable to copy data: {}, {}", e.kind(), e),
+            std::io::ErrorKind::ConnectionReset => {
+                log::debug!("client {} connection reset", peer_name);
+            }
+            _ => log::error!("Unable to copy data: {}, {}", e.kind(), e),
         },
     }
 }
 
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// Host / port to listen on
+    #[arg(short, long, default_value = "0.0.0.0:8080")]
+    addr: String,
+
+    /// Thread pool size
+    #[arg(short, long, default_value = "4")]
+    pool_size: usize,
+
+    #[clap(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
+}
+
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:8080").expect("couldn't start TCP server");
-    let pool = ThreadPool::new(4);
+    let cli_args = Args::parse();
+
+    env_logger::Builder::new()
+        .filter_level(cli_args.verbose.log_level_filter())
+        .init();
+
+    let listener = TcpListener::bind(&cli_args.addr).expect("couldn't start TCP server");
+    log::info!("server listening on {}", &cli_args.addr);
+
+    let pool = ThreadPool::new(cli_args.pool_size);
     // accept connections and process them serially
     for stream in listener.incoming() {
         // thread::spawn(|| {
@@ -35,5 +68,6 @@ fn main() -> std::io::Result<()> {
         // handle_client(stream?);
         pool.execute(|| handle_client(stream.expect("Unable to unwrap stream")));
     }
+
     Ok(())
 }
