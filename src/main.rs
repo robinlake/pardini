6484e2371ee9d4a80eb457fa58312d0e3ac18597
Max Edmands <max@honeycomb.io>
2023-06-25 15:27:27 -0700

    Organize code into modules

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,5 @@
 use clap::Parser;
-use pardini::ThreadPool;
-use std::{
-    fs::OpenOptions,
-    io::{copy, BufReader},
-    net::{TcpListener, TcpStream},
-};
-
-fn handle_client(stream: TcpStream) {
-    let peer_name = stream
-        .peer_addr()
-        .and_then(|ok| Ok(ok.to_string()))
-        .unwrap_or("Unknown".to_string());
-
-    log::debug!("client {} connected", peer_name);
-
-    let mut file = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open("gpslog.txt")
-        .expect("Unable to open file");
-
-    let mut reader = BufReader::new(&stream);
-    match copy(&mut reader, &mut file) {
-        Ok(_) => log::debug!("client {} disconnected", peer_name),
-        Err(e) => match e.kind() {
-            std::io::ErrorKind::ConnectionReset => {
-                log::debug!("client {} connection reset", peer_name);
-            }
-            _ => log::error!("Unable to copy data: {}, {}", e.kind(), e),
-        },
-    }
-}
+use pardini::tcp_server::start_server;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
@@ -49,25 +18,9 @@ struct Args {
 
 fn main() -> std::io::Result<()> {
     let cli_args = Args::parse();
-
     env_logger::Builder::new()
         .filter_level(cli_args.verbose.log_level_filter())
         .init();
-
-    let listener = TcpListener::bind(&cli_args.addr).expect("couldn't start TCP server");
-    log::info!("server listening on {}", &cli_args.addr);
-
-    let pool = ThreadPool::new(cli_args.pool_size);
-    // accept connections and process them serially
-    for stream in listener.incoming() {
-        // thread::spawn(|| {
-        //     // connection succeeded
-        //     let stream = stream.expect("Unable to unwrap stream");
-        //     handle_client(stream);
-        // });
-        // handle_client(stream?);
-        pool.execute(|| handle_client(stream.expect("Unable to unwrap stream")));
-    }
-
+    start_server(cli_args.addr, cli_args.pool_size);
     Ok(())
 }
---
+++ src/tcp_server.rs
@@ -0,0 +1,50 @@
+use crate::thread_pool::ThreadPool;
+use std::{
+    fs::OpenOptions,
+    io::{copy, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+pub fn start_server(addr: String, pool_size: usize) {
+    let listener = TcpListener::bind(&addr).expect("couldn't start TCP server");
+    log::info!("server listening on {}", &addr);
+
+    let pool = ThreadPool::new(pool_size);
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        // thread::spawn(|| {
+        //     // connection succeeded
+        //     let stream = stream.expect("Unable to unwrap stream");
+        //     handle_client(stream);
+        // });
+        // handle_client(stream?);
+        pool.execute(|| handle_client(stream.expect("Unable to unwrap stream")));
+    }
+}
+
+fn handle_client(stream: TcpStream) {
+    let peer_name = stream
+        .peer_addr()
+        .and_then(|ok| Ok(ok.to_string()))
+        .unwrap_or("Unknown".to_string());
+
+    log::debug!("client {} connected", peer_name);
+
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("gpslog.txt")
+        .expect("Unable to open file");
+
+    let mut reader = BufReader::new(&stream);
+    if let Err(e) = copy(&mut reader, &mut file) {
+        match e.kind() {
+            std::io::ErrorKind::ConnectionReset => {
+                log::debug!("client {} connection reset", peer_name)
+            }
+            _ => log::error!("Unable to copy data: {}, {}", e.kind(), e),
+        }
+    } else {
+        log::debug!("client {} disconnected", peer_name);
+    }
+}
